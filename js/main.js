090f67c0ac77a575193ee93ec1aa33b0b8e1e7ea
SHUN TAKAMA <shun.takama0206@gmail.com>
2026-06-05 13:27:29 +0900

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { initAccordion } from "./modules/accordion.js";
-import { initFadein } from "./modules/fadein.js";
-import { initPagetop } from "./modules/pagetop.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  initAccordion();
-  initFadein();
-  initPagetop();
-});
